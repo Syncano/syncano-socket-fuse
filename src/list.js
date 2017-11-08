1fb99d33f0f40eaa0a476ce3ee822de4b351cb54
Robert Kopaczewski <rk@23doors.com>
2017-11-08 10:52:26 +0100

    small refactor

--- src/list.js
+++ src/list.js
@@ -1,22 +1,29 @@
 import Server from 'syncano-server'
+
 export default async ctx => {
   const {data, response} = Server(ctx)
+
   try {
     const {user} = ctx.meta
     const {appid = null, entity = null, secret = false} = ctx.args
     const userId = user ? user.id : null
-    let list = data.transaction.where('appid', appid).where('entity', entity)
+
+    let query = data.transaction.where('appid', appid).where('entity', entity)
+
     if (userId !== null && secret === true) {
-      list = list.where('user', userId)
+      query = query.where('user', userId)
     }
-    list = await list.list()
-    list = list.filter(l => {
-      if (l.user !== null) {
-        return l.user === userId
+
+    transactions = await query.list()
+    transactions = transactions.filter(t => {
+      if (t.user !== null) {
+        return t.user === userId
       }
+
       return true
     })
-    return response.json(list)
+    return response.json(transactions)
+
   } catch (error) {
     return response.json({message: error.message}, 400)
   }
--- src/sync.js
+++ src/sync.js
@@ -1,6 +1,8 @@
 import Server from 'syncano-server'
+
 export default async ctx => {
   const {data, response, channel} = Server(ctx)
+
   try {
     const {user} = ctx.meta
     const {
@@ -12,16 +14,20 @@ export default async ctx => {
       secret = false,
       latestTid = null
     } = ctx.args
-    let lock = data.lock.where('appid', appid).where('entity', entity)
+
+    let query = data.lock.where('appid', appid).where('entity', entity)
     let userId = user ? user.id : null
+
     if (secret === true && userId === null) {
       throw new Error('If creating a secret object you must be logged in')
     }
     if (userId !== null && secret === true) {
-      lock = lock.where('user', userId)
+      query = query.where('user', userId)
     }
-    lock = await lock.list()
+
+    lock = await query.list()
     if (lock.length === 0) {
+      // Create new lock if it doesn't exit
       let lockParams = {
         appid,
         entity,
@@ -33,16 +39,19 @@ export default async ctx => {
       lock = [await data.lock.create(lockParams)]
     }
     lock = lock[0]
+
     if (latestTid === null && lock.latestTid !== tid) {
       throw new Error('Please provide last id')
     }
     if (latestTid !== null && lock.latestTid !== latestTid) {
       throw new Error('Transaction id mismatch')
     }
+
     await data.lock.update(lock.id, {
       expected_revision: lock.revision,
       latestTid: tid
     })
+
     let params = {
       appid,
       entity,
@@ -58,12 +67,15 @@ export default async ctx => {
         user: userId
       }
     }
+
     const createdTransaction = await data.transaction.create(params)
     const messagesString = secret === true && userId !== null
       ? `user_messages.${appid}.${userId}`
       : `messages.${appid}`
+
     channel.publish(messagesString, {entity, action, payload, tid})
     return response.json(createdTransaction)
+
   } catch ({message}) {
     return response.json(message, 409)
   }
